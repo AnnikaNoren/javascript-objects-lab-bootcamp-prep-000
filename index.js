0cd6b1a06c34e9421999a033fb51a71092862880
Annika Noren <amenoren@gmail.com>
2017-09-22 20:07:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newRecipes = Object.assign({}, object);
+  var newRecipes = recipes.assign({}, object);
   delete newRecipes[key]
   return object;
 }
